36757af6407b5414f33191f3129cdfe8f8f03940
Neemagrg <neemagurung1013@gmail.com>
2026-08-31 13:24:48 +0545

    Success

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,8 +2,7 @@ import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
 
-    const token =
-        localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
     if (!token) {
         return <Navigate to="/login" />;
